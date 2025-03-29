46a6bec279dcc0d42894b0d11622c0659a986c50
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-29 02:44:39 +0200

    Update SeriesClient.java

--- src/main/java/bg/stream_mates/backend/feather/series/client/SeriesClient.java
+++ src/main/java/bg/stream_mates/backend/feather/series/client/SeriesClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "stream-mate-series-svc", url = "https://stream-mate-series.onrender.com", configuration = FeignConfig.class)
+@FeignClient(name = "stream-mate-series-svc", url = "https://rich-babs-stream-mate-series-9d8a10c3.koyeb.app", configuration = FeignConfig.class)
 public interface SeriesClient {
 
     @GetMapping("/get-next-thirty-series")
