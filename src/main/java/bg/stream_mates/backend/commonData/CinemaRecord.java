2f89d3ed7d80e2aedc9533191ded986c79bb2285
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-29 22:01:43 +0200

    make it to be dto

--- src/main/java/bg/stream_mates/backend/commonData/CinemaRecord.java
+++ src/main/java/bg/stream_mates/backend/commonData/CinemaRecord.java
@@ -15,38 +15,26 @@ import java.time.Instant;
 @MappedSuperclass
 public abstract class CinemaRecord {
 
-    @Column(nullable = false)
     @Size(min = 2)
     private String title;
 
-    @Column(name = "poster_img_URL", nullable = false)
     @Size(min = 8)
     private String posterImgURL;
 
-    @Column(name = "special_text")
     private String specialText;
 
-    @Column(name = "search_tag", nullable = false)
     private String searchTag;
 
-    @Column(nullable = false)
     private String genres;  // може да бъде null
 
-    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(name = "release_date")
     private String releaseDate;  // може да бъде null
 
-    @Column(name = "tmdb_rating")
     private String tmdbRating;
 
-    @Column(name = "background_img_URL")
     @Size(min = 10)
     private String backgroundImg_URL;
 
-    @Column(name = "created_at", nullable = false)
     private Instant createdAt;
-
-    // Други общи полета за медийните обекти, като description, year и т.н.
 }
